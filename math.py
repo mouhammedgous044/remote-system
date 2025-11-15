93a2f191b347f713cbf121e5e0fa093e4808cf88
mouhammedgous044 <jirayatmohammedgousulayan@gmail.com>
2025-11-15 08:53:53 +0530

    Add basic math operations for user input

---
+++ math.py
@@ -0,0 +1,10 @@
+#math.py
+try:
+  a=float(input("Enter first number:"))
+  b=float(input("Enter second number:"))
+
+  print(f"Addition:{a+b}")
+  print(f"subtraction:{a-b}")
+except ValueError:
+  print("Please enter valid number.")
+
